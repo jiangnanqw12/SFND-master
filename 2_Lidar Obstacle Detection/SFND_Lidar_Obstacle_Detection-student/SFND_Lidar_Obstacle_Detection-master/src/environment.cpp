65453bebdf5d6ab003beac95048aa974d705ba7e
Harry <382961818@qq.com>
2021-03-19 22:21:35 +0800

    renderPointCloud

--- 2_Lidar Obstacle Detection/SFND_Lidar_Obstacle_Detection-student/SFND_Lidar_Obstacle_Detection-master/src/environment.cpp
+++ 2_Lidar Obstacle Detection/SFND_Lidar_Obstacle_Detection-student/SFND_Lidar_Obstacle_Detection-master/src/environment.cpp
@@ -42,14 +42,16 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
     // ----------------------------------------------------
 
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
 
     // TODO:: Create lidar sensor
     Lidar *Lidar1 = new Lidar(cars, 0);
     // TODO:: Create point processor
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = Lidar1->scan();
-    renderRays(viewer, Lidar1->position, cloud);
+    //renderRays(viewer, Lidar1->position, cloud);
+    //renderPointCloud(viewer, cloud, "t", Color(0, 1, 0));
+    renderPointCloud(viewer, cloud, "data");
 }
 
 // setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
