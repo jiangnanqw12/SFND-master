b2f878ad489d5057d6468e0c5a127aace7cdc3a0
shade <382961818@qq.com>
2021-08-10 12:57:27 +0800

    render search

--- 2_Lidar Obstacle Detection/SFND_Lidar_Obstacle_Detection-student/SFND_Lidar_Obstacle_Detection-master/src/environment.cpp
+++ 2_Lidar Obstacle Detection/SFND_Lidar_Obstacle_Detection-student/SFND_Lidar_Obstacle_Detection-master/src/environment.cpp
@@ -195,11 +195,10 @@ void cityBlock_project_student(pcl::visualization::PCLVisualizer::Ptr &viewer,
                                                Eigen::Vector4f(30, 8, 1, 1));
     //renderPointCloud(viewer, filterCloud, "filterCloud");
     std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->RansacPlane_student(filterCloud, 25, 0.3);
-    //renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-    //renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
     std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering_euclideanCluster(segmentCloud.first, 0.53, 10, 500, viewer);
-    KdTree_euclidean<pcl::PointXYZI> *tree;
-    //pointProcessorI->test(segmentCloud.first, tree);
+
     //view insert
     // int it = 0;
     // Box window;
@@ -211,18 +210,18 @@ void cityBlock_project_student(pcl::visualization::PCLVisualizer::Ptr &viewer,
     // window.z_max = 0;
     // render2DTree(tree->root, viewer, window, it);
 
-    // int clusterId = 0;
-    // std::vector<Color> colors = {Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1)};
-    // for (auto cloudcluster : cloudClusters)
-    // {
-    //     std::cout << "cluster size ";
-    //     pointProcessorI->numPoints(cloudcluster);
-    //     renderPointCloud(viewer, cloudcluster, "obstCloud" + std::to_string(clusterId),
-    //                      colors[clusterId % colors.size()]);
-    //     Box box = pointProcessorI->BoundingBox(cloudcluster);
-    //     renderBox(viewer, box, clusterId);
-    //     clusterId++;
-    // }
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1)};
+    for (auto cloudcluster : cloudClusters)
+    {
+        std::cout << "cluster size ";
+        pointProcessorI->numPoints(cloudcluster);
+        renderPointCloud(viewer, cloudcluster, "obstCloud" + std::to_string(clusterId),
+                         colors[clusterId % colors.size()]);
+        //Box box = pointProcessorI->BoundingBox(cloudcluster);
+        //renderBox(viewer, box, clusterId);
+        clusterId++;
+    }
 }
 // void cityBlock_solution(pcl::visualization::PCLVisualizer::Ptr &viewer,
 //                         ProcessPointClouds<pcl::PointXYZI> *pointProcessor,
@@ -331,16 +330,17 @@ int main(int argc, char **argv)
     //pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
     auto streamIterator = stream.begin();
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
-    //cityBlock(viewer);
-    //cityBlock_student(viewer, pointProcessorI, inputCloudI);
-    //cityBlock_solution(viewer, pointProcessorI, inputCloudI);
-    // Clear viewer
+
     viewer->removeAllPointClouds();
     viewer->removeAllShapes();
 
     // Load pcd and run obstacle detection process
     inputCloudI = pointProcessorI->loadPcd((*streamIterator).string());
     cityBlock_project_student(viewer, pointProcessorI, inputCloudI);
+    //cityBlock(viewer);
+    //cityBlock_student(viewer, pointProcessorI, inputCloudI);
+    //cityBlock_solution(viewer, pointProcessorI, inputCloudI);
+    // Clear viewer
     while (!viewer->wasStopped())
     {
 
