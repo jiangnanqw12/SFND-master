adec84c7861eb5981566f76ac19d14ace82c68fb
jiangnanqw12 <382961818@qq.com>
2021-11-29 13:50:32 +0800

    doppler_estimation_student2

---
+++ 4_Radar/03_Range-Doppler Estimation/doppler_estimation_student2.m
@@ -0,0 +1,12 @@
+% Doppler Velocity Calculation
+c = 3 * 10^8; %speed of light
+frequency = 77e9; %frequency in Hz
+
+% TODO : Calculate the wavelength
+wavelength = c / frequency;
+% TODO : Define the doppler shifts in Hz using the information from above
+frequency_shift = [3e3, -4.5e3, 11e3, -3e3];
+% TODO : Calculate the velocity of the targets  fd = 2*vr/lambda
+vr = frequency_shift * wavelength / 2;
+fd = 2 * vr / wavelength;
+% TODO: Display results
